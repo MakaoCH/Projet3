ce818021a57a64949c27079608aadf484fbb1471
Mickael Brothier <mickaelbrothier@outlook.com>
2023-02-03 16:41:26 +0100

    Fin de l'étape 1, ajout des travaux, des filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,6 +2,7 @@
 
 const works = await fetch ('http://localhost:5678/api/works').then(reponse => reponse.json());
 
+function genererWorks(works) {
     for (let i = 0; i < works.length; i++) {
 
     const fiche = document.createElement("figure");
@@ -17,29 +18,44 @@ const works = await fetch ('http://localhost:5678/api/works').then(reponse => re
     fiche.appendChild(images);
     fiche.appendChild(titre);
     }
+  }
+// Premier affichage de la page
+genererWorks(works);
   
  //Ajout des filtres//
   
  
   const boutonTous = document.querySelector("#btn-tous");
     boutonTous.addEventListener ("click", function () {
-    const filtresTous = works.filter(nom => nom.categoryId !== 0)
-    console.log(filtresTous);        
-  });   
+    const filtresTous = works.filter(id => id.categoryId !== 0)
+    
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(filtresTous);       
+  });  
+
   const boutonObjet = document.querySelector("#btn-objets");
     boutonObjet.addEventListener ("click", function () {
-    const filtresObjets = works.filter(nom => nom.categoryId === 1)
-    console.log(filtresObjets);      
+    const filtresObjets = works.filter(id => id.categoryId === 1)   
+  
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(filtresObjets);
   });
+
+
   const boutonAppt = document.querySelector("#btn-appartements");
     boutonAppt.addEventListener ("click", function () {
-    const filtresAppt = works.filter(nom => nom.categoryId === 2)
-    console.log(filtresAppt);        
+    const filtresAppt = works.filter(id => id.categoryId === 2)
+   
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(filtresAppt);       
   });
+
   const boutonHotelResto = document.querySelector("#btn-hotelresto");
     boutonHotelResto.addEventListener ("click", function () {
-    const filtresHotelResto = works.filter(nom => nom.categoryId === 3)
-    console.log(filtresHotelResto);        
+    const filtresHotelResto = works.filter(id => id.categoryId === 3)
+  
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(filtresHotelResto);       
   });
     
    
