0d2556cfb0795d1529e9aff5bf47de75699581a5
Mickael Brothier <mickaelbrothier@outlook.com>
2023-02-14 17:20:32 +0100

    Modal suite

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -120,16 +120,72 @@ genererFiltres(filtres)
    });
   
   //modal
+
+let modal = null
+
 const openModal = function (e) {
    e.preventDefault()
    const target = document.querySelector(e.target.getAttribute("data-id"))
    target.style.display = null
    target.removeAttribute('aria-hidden')
    target.setAttribute('aria-modal', 'true')
+   modal = target
+   modal.addEventListener("click", closeModal)
+   modal.querySelector(".js-close-modal").addEventListener("click", closeModal) 
+   modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation) 
 }
 
-  document.querySelectorAll('.btn-modifier').forEach(a => {
+const closeModal = function (e) {
+   e.preventDefault()
+   if (modal === null) return
+   modal.style.display = "none"
+   modal.setAttribute('aria-hidden', 'true')
+   modal.removeAttribute('aria-modal')
+   modal.removeEventListener("click", closeModal)
+   modal.querySelector(".js-close-modal").removeEventListener("click", closeModal) 
+   modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation) 
+   modal = null
+}
+
+const stopPropagation = function (e) {
+   e.stopPropagation()
+}
+
+document.querySelectorAll('.btn-modifier').forEach(a => {
    a.addEventListener('click', openModal)
    
-  })
-  
+})
+
+//galeries modal
+async function genererWorksModal(worksModal) {
+   const mesProjetsModal = document.querySelector(".gallery-modal");
+   mesProjetsModal.innerHTML = "";
+   for (let i = 0; i < worksModal.length; i++) {
+     const imageWrapper = document.createElement("div");
+     imageWrapper.classList.add("image-wrapper");
+ 
+     const image = document.createElement("img");
+     image.src = worksModal[i].imageUrl;
+     image.setAttribute("crossorigin", "anonymous");
+ 
+     const deleteButton = document.createElement("button");
+     deleteButton.innerHTML = '<i class="fa-solid fa-trash-can fa-xs"></i>';
+     deleteButton.classList.add("delete-button");
+
+      
+     deleteButton.addEventListener("click", () => {
+       imageWrapper.remove();
+     });
+ 
+     imageWrapper.appendChild(image);
+     imageWrapper.appendChild(deleteButton);
+     mesProjetsModal.appendChild(imageWrapper);
+
+   }
+ }
+ 
+ 
+ const worksModal = await fetch('http://localhost:5678/api/works').then(response => response.json());
+ genererWorksModal(worksModal);
+ 
+ 
